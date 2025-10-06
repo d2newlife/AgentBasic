2cdd7a55e675c3ea0302214dc5563c950e656c9a
d2newlife <allsec2020dg@gmail.com>
2025-10-05 21:07:29 -0400

    Setup

---
+++ main.py
@@ -0,0 +1,6 @@
+
+def myfunction(*args, **kwargs):   # Accept any number of positional values
+    print(args, kwargs)
+myfunction (1,2, x=1, s="hello", b=True)  #(1, 2) {'x': 1, 's': 'hello', 'b': True}  Now there is a Tuple and a Dictionary
+
+
